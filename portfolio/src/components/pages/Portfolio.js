3d5da9a8a4231276ab0dd33232a5fb36124ab87f
Choogi <drewtex@gmail.com>
2022-12-19 16:22:08 -0600

    completed portfolio page

--- portfolio/src/components/pages/Portfolio.js
+++ portfolio/src/components/pages/Portfolio.js
@@ -11,38 +11,32 @@ export default function Portfolio() {
       </p>
       <section className="binky">
           <div className="container" display='flex'>
-            <img src={"jukebox.PNG"} alt='A Jukebox'  className="image" style={{width:'100%'}}></img>
+            <p></p>
             <div className="middle">
-            <button className="text" style={{width:'100%'}}>Github</button>
-            <button className="text" style={{width:'100%'}}>Deployed Site</button>
+            <a href='https://github.com/Chueg/Jukebox-vite'><button className="text" style={{width:'100%'}}>Github</button></a>
+            <a href='https://jukebox-project3.herokuapp.com/'><button className="text" style={{width:'100%'}}>Deployed Site</button></a>
             </div>
+            <img src={"jukebox.PNG"} alt='A Jukebox'  className="image" style={{width:'100%'}}></img>
+            
             
           </div>
           <div className="container">
             <img src={"recipe.PNG"} alt='The recipeIO Logo' className="image" style={{width:'100%'}}></img>
             <div className="middle">
-            <button className="text" style={{width:'100%'}}>Github</button>
-            <button className="text" style={{width:'100%'}}>Deployed Site</button>
+              <a href='https://github.com/Lawhornmatt/RecipeIO'><button className="text" style={{width:'100%'}}>Github</button></a>
+            
+              <a href='http://recipeio-project2.herokuapp.com/'><button className="text" style={{width:'100%'}}>Deployed Site</button></a>
             </div>
             
           </div>
           <div className="container">
-            <img src={"zerblor.PNG"} alt='zerblor' className="image" style={{width:'100%'}}></img>
-            <div className="middle">
-            <button className="text">John Doe</button>
+          <div className="middle">
+            <a href='https://github.com/Chueg/ReactPortfolio'><button className="text" style={{width:'100%'}}>Github</button></a>
             </div>
-            
-          </div>
-          <div className="container">
-            <img src={"zerblor.PNG"} alt='zerblor' className="image" style={{width:'100%'}}></img>
-            <div className="middle">
-            <button className="text">John Doe</button>
-            </div>
+            <img src={"porto.gif"} alt='zerblor' className="image" style={{width:'100%'}}></img>
             
           </div>
       </section>
-     
-        
      </div>
   );
 }
