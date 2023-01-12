31d06e4e324d5bca0ae4170a321f853719911746
Choogi <drewtex@gmail.com>
2023-01-12 12:52:23 -0600

    text fix and logo sizes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ export default function About() {
     <div>
       <h1>About Me</h1>
       <p className="stinky">
-      I'm a Full Stack web developer leveraging computing background to build a more intuitive user experience on the web. Recently earned a certificate in full stack development from the University Coding, with newly developed skills in JavaScript, CSS, React.js, and responsive web design.
+      I'm a Full Stack web developer leveraging computing background to build a more intuitive user experience on the web. Recently earned a certificate in full stack development from the University of Texas Coding Bootcamp, with newly developed skills in JavaScript, CSS, React.js, and responsive web design.
       </p>
     </div>
   );
