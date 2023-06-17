c9607b7327e0e7c738835ddd2ed266fb16049764
Smokems <drewtex@gmail.com>
2023-06-17 13:07:16 -0500

    typo

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>Abutt Me</h1>
+      <h1>About Me</h1>
       <p className="stinky">
       I'm a Full Stack web developer leveraging computing background to build a more intuitive user experience on the web. Recently earned a certificate in full stack development from the University of Texas Coding Bootcamp, with newly developed skills in JavaScript, CSS, React.js, and responsive web design.
       </p>
